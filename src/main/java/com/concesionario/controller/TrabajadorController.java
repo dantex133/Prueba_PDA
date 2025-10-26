0e4e469333f3184fab9ae16cf33e420184d1bb16
Jorge <jorgegod2025@gmail.com>
2025-10-25 21:15:36 -0500

    Mejoras a perfil analista, carga de datos reales, para vizualizacion y limpieza de informacion de deroracion

--- src/main/java/com/concesionario/controller/TrabajadorController.java
+++ src/main/java/com/concesionario/controller/TrabajadorController.java
@@ -1,20 +1,48 @@
 package com.concesionario.controller;
 
-
+import com.concesionario.repository.UsuarioRepository;
+import com.concesionario.repository.VehiculoRepository;
+import com.concesionario.repository.CitaRepository;
+import com.concesionario.repository.TrabajadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 public class TrabajadorController {
 
-    @GetMapping("/perfil_gestor")
-    public String perfilG() {
-        return "Perfil_gestor"; // Sin carpeta
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private VehiculoRepository vehiculoRepository;
+
+    @Autowired
+    private CitaRepository citaRepository;
+
+    @Autowired
+    private TrabajadorRepository trabajadorRepository;
 
     @GetMapping("/perfil_analisis")
-    public String perfilA() {
-        return "Perfil_analisis"; // Sin carpeta
+    public String perfilA(Model model) {
+
+        long totalClientes = usuarioRepository.count();
+        long totalVehiculos = vehiculoRepository.count();
+        long totalCitas = citaRepository.count();
+        long totalTrabajadores = trabajadorRepository.count();
+
+
+        model.addAttribute("totalClientes", totalClientes);
+        model.addAttribute("totalVehiculos", totalVehiculos);
+        model.addAttribute("totalCitas", totalCitas);
+        model.addAttribute("totalTrabajadores", totalTrabajadores);
+
+        return "Perfil_analisis";
+    }
+
+    @GetMapping("/perfil_gestor")
+    public String perfilG() {
+        return "Perfil_gestor";
     }
 }
